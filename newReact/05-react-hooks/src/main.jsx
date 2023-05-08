ca2a324616f1821788021f8727a075e3be65058e
edcastillob <edwar.castillo@gmail.com>
2023-05-08 14:14:09 -0400

    Final Section

--- newReact/05-react-hooks/src/main.jsx
+++ newReact/05-react-hooks/src/main.jsx
@@ -11,7 +11,8 @@ import './index.css'
 // import { Layout } from './05-useLayoutEffect/Layout';
 //import { Memorize } from './06-memos/Memorize';
 //import { CallbackHook } from './06-memos/CallbackHook';
-import { MemoHook } from './06-memos/MemoHook';
+//import { MemoHook } from './06-memos/MemoHook';
+import { Padre } from './07-tarea-memo/Padre';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -24,8 +25,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     //<FocusScreen/>
     //<Layout/>
     //<Memorize/>
-    <MemoHook/>
+    //<MemoHook/>
    // <CallbackHook/>
+   <Padre/>
+
 
 
 
