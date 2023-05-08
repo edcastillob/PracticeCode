72188b0a4850bf86f009feeed36943dbcd542f41
edcastillob <edwar.castillo@gmail.com>
2023-05-08 09:34:38 -0400

    useMemo

--- newReact/05-react-hooks/src/main.jsx
+++ newReact/05-react-hooks/src/main.jsx
@@ -10,7 +10,8 @@ import './index.css'
 //import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
 //import { Memorize } from './06-memos/Memorize';
-import { CallbackHook } from './06-memos/CallbackHook';
+//import { CallbackHook } from './06-memos/CallbackHook';
+import { MemoHook } from './06-memos/MemoHook';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -23,7 +24,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     //<FocusScreen/>
     //<Layout/>
     //<Memorize/>
-    <CallbackHook/>
+    <MemoHook/>
+   // <CallbackHook/>
 
 
 
