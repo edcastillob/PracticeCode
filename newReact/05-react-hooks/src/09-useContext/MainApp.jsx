213e40b49725456a0eadd03aff4f0e082a53ad7c
edcastillob <edwar.castillo@gmail.com>
2023-05-10 15:54:41 -0400

    UseContext

---
+++ newReact/05-react-hooks/src/09-useContext/MainApp.jsx
@@ -0,0 +1,28 @@
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { LoginPage } from "./LoginPage";
+import { AboutPage } from "./AboutPage";
+
+
+
+export const MainApp = () => {
+  return (
+    <>
+        <h1>MainApp</h1>
+        <hr />
+
+        <Routes>
+        <Route path="/"  element={<HomePage/>}/>
+        <Route path="about"  element={<AboutPage/>}/>
+        <Route path="login"  element={<LoginPage/>}/>
+
+        {/* <Route path="l/*"  element={<LoginPage/>}/> */}
+        <Route path="/*"  element={ <Navigate to="/about" /> }/>
+
+
+
+        </Routes>
+    </>
+  )
+}
