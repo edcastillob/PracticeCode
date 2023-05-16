aa58babf96071d35a72706ca8eb278a89d2931b4
edcastillob <edwar.castillo@gmail.com>
2023-05-16 17:24:21 -0400

    NavBar

---
+++ newReact/07-hero-spa/src/heroes/data/heroes.js
@@ -0,0 +1,66 @@
+export const heroes = [
+    {
+        'id': 'dc-batman',
+        'superhero':'Batman',
+        'publisher':'DC Comics',
+        'alter_ego':'Bruce Wayne',
+        'first_appearance':'Detective Comics #27',
+        'characters':'Bruce Wayne'
+    },
+    {
+        'id': 'dc-wonderW',
+        'superhero':'Mujer Maravilla',
+        'publisher':'DC Comics',
+        'alter_ego':'Fernanda Castillo',
+        'first_appearance':'La super hija #06',
+        'characters':'FerCastillo'
+    },
+    {
+        'id': 'dc-wonderM',
+        'superhero':'Madre Maravilla',
+        'publisher':'DC Comics',
+        'alter_ego':'Karlins Medina',
+        'first_appearance':'La super esposa #22',
+        'characters':'KarMedina'
+    },
+    {
+        'id': 'dc-EdCast',
+        'superhero':'Super Papá',
+        'publisher':'DC Comics',
+        'alter_ego':'Edwar Castillo',
+        'first_appearance':'El super papá #22',
+        'characters':'EdCastillo'
+    },
+    {
+        'id': 'marvel-Abuu',
+        'superhero':'Abuela-Juana',
+        'publisher':'Marvel Comics',
+        'alter_ego':'Juana Blazco',
+        'first_appearance':'Tamborera #44',
+        'characters':'JBautista'
+    },
+    {
+        'id': 'marvel-mudito',
+        'superhero':'Super Emilio',
+        'publisher':'Marvel Comics',
+        'alter_ego':'Emilio Gonzalez',
+        'first_appearance':'Carnicero #30',
+        'characters':'MMG'
+    },
+    {
+        'id': 'marvel-Murry',
+        'superhero':'Murry Curry',
+        'publisher':'Marvel Comics',
+        'alter_ego':'Delia Gonz',
+        'first_appearance':'Murry Curry #66',
+        'characters':'DeliaGonz'
+    },
+    {
+        'id': 'marvel-Mamelon',
+        'superhero':'Niña Grande',
+        'publisher':'Marvel Comics',
+        'alter_ego':'Yohana Lucam',
+        'first_appearance':'Mamelon #12',
+        'characters':'YohanaMame'
+    }
+]
--- newReact/07-hero-spa/src/heroes/index.js
+++ newReact/07-hero-spa/src/heroes/index.js
@@ -1,7 +1,7 @@
 
 
     //export * from './components';
-    //export * from './helpers';
+    export * from './helpers';
     //export * from './hooks';
     export * from './pages';
     export * from './routes/HeroesRoutes';
